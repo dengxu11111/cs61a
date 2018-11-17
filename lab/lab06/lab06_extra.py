59875256e3defcbb65822745b1bbaeb2ca6774d7
dengxu11111 <373543804@qq.com>
2018-11-17 12:12:06 +0800

    pro ants

---
+++ lab/lab06/lab06_extra.py
@@ -0,0 +1,32 @@
+""" Optional problems for Lab 6 """
+
+## Nonlocal practice
+def vending_machine(snacks):
+    """Cycles through sequence of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    "*** YOUR CODE HERE ***"
+    count=0
+    def h():
+        nonlocal count
+        a=snacks[count%len(snacks)]
+        count+=1
+        return a
+        
+
+
+    return h
